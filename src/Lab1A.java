05f533cf75f1009f04447cf35d3f7b8cde58aee1
voksepp <fredrik.voksepp@gmail.com>
2016-11-07 14:24:21 +0100

    Initial commit

--- src/Lab1A.java
+++ src/Lab1A.java
@@ -32,7 +32,7 @@ public class Lab1A {
         int[] intArray = new int[integerList.size()];
 
         for (int i=0; i < intArray.length; i++)
-            intArray[i] = integerList.get(i);                                   // unboxing
+            intArray[i] = integerList.get(i);                                   // implied unboxing
 
         return intArray;
     }
